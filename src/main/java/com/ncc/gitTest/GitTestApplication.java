07c9673db243f1bc7ba1ba380b6eeaa88f97c472
ncc <yikemuliang@163.com>
2022-07-16 16:41:18 +0800

    feat:添加hello方法和配置datasource数据源

--- src/main/java/com/ncc/gitTest/GitTestApplication.java
+++ src/main/java/com/ncc/gitTest/GitTestApplication.java
@@ -2,12 +2,19 @@ package com.ncc.gitTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GitTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GitTestApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "hello, world, 刘佳乐";
+	}
 }
